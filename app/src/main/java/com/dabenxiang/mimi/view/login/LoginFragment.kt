e2463bd3b393469577addaa5a409777fa8508b48
James <james.cheng@silkrode.com.tw>
2020-04-06 11:31:09 +0800

    modifying login UI adding some icons

--- app/src/main/java/com/dabenxiang/mimi/view/login/LoginFragment.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/login/LoginFragment.kt
@@ -1,16 +1,50 @@
 package com.dabenxiang.mimi.view.login
 
+import android.Manifest
 import android.os.Bundle
 import android.view.View
 import com.dabenxiang.mimi.R
 import com.dabenxiang.mimi.view.base.BaseFragment
+import org.koin.android.viewmodel.ext.android.viewModel
 
-class LoginFragment: BaseFragment() {
+class LoginFragment: BaseFragment(), OnPermissionListener {
+
+    private val viewModel by viewModel<LoginViewModel>()
+
+    companion object {
+        const val PERMISSION_REQUEST_CODE = 100
+        const val INSTALL_REQUEST_CODE = 200
+    }
+
+    private val permissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.CAMERA,
+        Manifest.permission.READ_PHONE_STATE
+    )
+
+//    private var dialog: LoginDialogFragment? = null
+//    private val onLoginDialogListener = object : OnLoginDialogListener {
+//        override fun onSuccess() {
+//            dialog?.dismiss()
+//            goHome()
+//        }
+//    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
 
     override fun getLayoutId(): Int {
         return  R.layout.fragment_login
+    }
+
+    override fun onAllPermissionsGranted() {
+        // todo
+    }
+
+    private fun goHome() {
+        // todo
     }
 }
--- app/src/main/java/com/dabenxiang/mimi/view/login/LoginViewModel.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/login/LoginViewModel.kt
@@ -1,5 +1,22 @@
 package com.dabenxiang.mimi.view.login
 
+import androidx.lifecycle.viewModelScope
 import com.dabenxiang.mimi.view.base.BaseViewModel
 
-class LoginViewModel : BaseViewModel()
+class LoginViewModel : BaseViewModel() {
+
+//    fun autoLogin() {
+//        when {
+//            accountManager.isAutoLogin() -> {
+//                viewModelScope.launch {
+//                    val profile = accountManager.getProfile()
+//                    accountManager.login(profile!!.account, profile.password)
+//                        .collect { _loginResult.value = it }
+//                }
+//            }
+//            else -> {
+//                _isAutoLogin.value = false
+//            }
+//        }
+//    }
+}
