ee3a5461426ea213925b34679151fdcbabf94180
Jeff.Yu <jeffyu925@gmail.com>
2020-05-06 16:20:39 +0800

    Add NavigationExtensions

--- app/src/main/java/com/dabenxiang/mimi/view/main/MainActivity.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/main/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.dabenxiang.mimi.view.main
 
-import android.content.res.Configuration
-import android.content.res.Resources
 import android.os.Bundle
-import android.util.DisplayMetrics
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.Observer
-import androidx.navigation.Navigation
 import com.dabenxiang.mimi.R
 import com.dabenxiang.mimi.view.base.BaseActivity
+import com.dabenxiang.mimi.widget.utility.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -24,16 +21,7 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        windowManager?.let {
-            val metrics = DisplayMetrics()
-            it.defaultDisplay.getMetrics(metrics)
-            metrics.density = metrics.ydpi / 160
-            metrics.densityDpi = metrics.ydpi.toInt()
-            metrics.scaledDensity = metrics.density
-            resources.configuration.densityDpi = metrics.densityDpi
-            resources.configuration.fontScale = 1f
-            baseContext.resources.updateConfiguration(resources.configuration, metrics)
-        }
+        setupBottomNavigationBar()
 
         viewModel.loadHomeCategories()
 
@@ -44,53 +32,20 @@ class MainActivity : BaseActivity() {
                 delegate.localNightMode = mode
             }
         })
-
-        bottom_navigation.setOnNavigationItemReselectedListener {}
-
-        bottom_navigation.setOnNavigationItemSelectedListener {
-            return@setOnNavigationItemSelectedListener when (it.itemId) {
-                R.id.btn_nav_home -> {
-                    Navigation.findNavController(this, R.id.nav_host_fragment)
-                        .navigate(R.id.action_bottom_nav_to_homeFragment)
-                    true
-                }
-
-                R.id.btn_nav_topup -> {
-                    Navigation.findNavController(this, R.id.nav_host_fragment)
-                        .navigate(R.id.action_bottom_nav_to_topupFragment)
-                    true
-                }
-
-                R.id.btn_nav_favorite -> {
-                    Navigation.findNavController(this, R.id.nav_host_fragment)
-                        .navigate(R.id.action_bottom_nav_to_favoriteFragment)
-                    true
-                }
-
-                R.id.btn_nav_personal -> {
-                    Navigation.findNavController(this, R.id.nav_host_fragment)
-                        .navigate(R.id.action_bottom_nav_to_personalFragment)
-                    true
-                }
-                else -> false
-            }
-        }
     }
 
-    override fun getResources(): Resources {
-        val overrideConfiguration = baseContext.resources.configuration
-        if (overrideConfiguration.fontScale != 1f) {
-            overrideConfiguration.fontScale = 1f
-            val context = createConfigurationContext(overrideConfiguration)
-            return context.resources
-        }
-        return super.getResources()
-    }
+    /**
+     * Called on first creation and when restoring state.
+     */
+    private fun setupBottomNavigationBar() {
+        val navGraphIds = listOf(R.navigation.navigation_home, R.navigation.navigation_topup, R.navigation.navigation_favorite, R.navigation.navigation_personal)
 
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        if (newConfig.fontScale != 1f) {
-            resources
-        }
-        super.onConfigurationChanged(newConfig)
+        // Setup the bottom navigation view with a list of navigation graphs
+        bottom_navigation.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_fragment,
+            intent = intent
+        )
     }
 }
