c805c4b644afe722676464373497c0bef624378c
sion <sion.wang@silkrode.com.tw>
2020-07-09 21:05:21 +0800

    Applied playbackStateListener.

--- app/src/main/java/com/dabenxiang/mimi/view/adapter/ClipAdapter.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/adapter/ClipAdapter.kt
@@ -15,9 +15,7 @@ import com.dabenxiang.mimi.model.api.vo.ContentItem
 import com.dabenxiang.mimi.model.api.vo.MemberPostItem
 import com.dabenxiang.mimi.view.adapter.viewHolder.ClipViewHolder
 import com.dabenxiang.mimi.view.player.PlayerViewModel
-import com.google.android.exoplayer2.C
-import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.ext.rtmp.RtmpDataSourceFactory
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
@@ -28,6 +26,7 @@ import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_player.*
 import timber.log.Timber
 import java.io.File
 
@@ -60,6 +59,8 @@ class ClipAdapter(
             }
     }
 
+    private var currentViewHolder: ClipViewHolder? = null
+
     override fun getItemCount(): Int {
         return memberPostItems.count()
     }
@@ -76,7 +77,6 @@ class ClipAdapter(
 
     override fun onViewRecycled(holder: ClipViewHolder) {
         super.onViewRecycled(holder)
-        holder.playerView.player?.release()
     }
 
     fun updateCurrentPosition(position: Int) {
@@ -89,6 +89,8 @@ class ClipAdapter(
 
     override fun onBindViewHolder(holder: ClipViewHolder, position: Int) {
         Timber.d("onBindViewHolder position:$position, currentPosition: $currentPosition")
+        takeIf { currentPosition == position }?.also { currentViewHolder = holder }
+            ?: run { holder.playerView.visibility = View.GONE }
         val item = memberPostItems[position]
         val contentItem = Gson().fromJson(item.content, ContentItem::class.java)
         takeIf { contentItem.images.isNotEmpty() }?.also {
@@ -109,7 +111,6 @@ class ClipAdapter(
 
     private fun processClip(playerView: PlayerView, id: String, url: String, position: Int) {
         Timber.d("processClip position:$position")
-        takeUnless { currentPosition == position }?.also { playerView.visibility = View.GONE }
         val item = memberPostItems[position]
         val contentItem = Gson().fromJson(item.content, ContentItem::class.java)
         playerView.player?.also { it.playWhenReady = false }
@@ -134,6 +135,7 @@ class ClipAdapter(
                 player.repeatMode = Player.REPEAT_MODE_OFF
                 player.playWhenReady = true
                 player.volume = PlayerViewModel.volume
+                player.addListener(playbackStateListener)
             }
             val agent = Util.getUserAgent(context, context.getString(R.string.app_name))
             val sourceFactory = DefaultDataSourceFactory(context, agent)
@@ -144,8 +146,65 @@ class ClipAdapter(
                 }
             }
         }
-        playerView.visibility = View.VISIBLE
         playerView.player?.also { it.playWhenReady = true }
+    }
+
+    private val playbackStateListener = object : Player.EventListener {
+        override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+            val stateString: String = when (playbackState) {
+                ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE"
+                ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING"
+                ExoPlayer.STATE_READY -> {
+                    currentViewHolder?.playerView?.visibility = View.VISIBLE
+                    "ExoPlayer.STATE_READY"
+                }
+
+                ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED"
+                else -> "UNKNOWN_STATE"
+            }
+            Timber.d("Changed state to $stateString playWhenReady: $playWhenReady")
+        }
+
+        override fun onLoadingChanged(isLoading: Boolean) {
+            Timber.d("onLoadingChanged")
+        }
+
+        override fun onPositionDiscontinuity(reason: Int) {
+            Timber.d("onPositionDiscontinuity: $reason")
+        }
+
+        override fun onTimelineChanged(timeline: Timeline, reason: Int) {
+            Timber.d("onTimelineChanged")
+        }
+
+        override fun onPlayerError(error: ExoPlaybackException) {
+            when (error.type) {
+                ExoPlaybackException.TYPE_SOURCE -> {
+                    Timber.d("error: TYPE_SOURCE")
+                    //showErrorDialog("SOURCE")
+                }
+                ExoPlaybackException.TYPE_RENDERER -> {
+                    Timber.d("error: TYPE_RENDERER")
+                    //showErrorDialog("RENDERER")
+                }
+                ExoPlaybackException.TYPE_REMOTE -> {
+                    Timber.d("error: TYPE_REMOTE")
+                    //showErrorDialog("REMOTE")
+                }
+                ExoPlaybackException.TYPE_OUT_OF_MEMORY -> {
+                    Timber.d("error: OUT_OF_MEMORY")
+                    //showErrorDialog("OUT_OF_MEMORY")
+                }
+                ExoPlaybackException.TYPE_UNEXPECTED -> {
+                    Timber.d("error: TYPE_UNEXPECTED")
+                    //showErrorDialog("UNEXPECTED")
+                }
+                else -> {
+                    Timber.d("error: UNKNOWN")
+                    //showErrorDialog("UNKNOWN")
+                }
+            }
+        }
     }
 
     private fun getMediaSource(
