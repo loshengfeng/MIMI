d335605c49ab827f5bb1da125a6e421cd7b5ffd8
sion <sion.wang@silkrode.com.tw>
2020-07-14 11:28:52 +0800

    Applied like and favorite api.

--- app/src/main/java/com/dabenxiang/mimi/view/clip/ClipViewHolder.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/clip/ClipViewHolder.kt
@@ -5,7 +5,9 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.dabenxiang.mimi.R
 import com.dabenxiang.mimi.model.api.vo.MemberPostItem
+import com.dabenxiang.mimi.model.enums.LikeType
 import com.google.android.exoplayer2.ui.PlayerView
 import kotlinx.android.synthetic.main.item_clip.view.*
 
@@ -20,7 +22,7 @@ class ClipViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     var tvLike: TextView = view.tv_like
     var tvComment: TextView = view.tv_comment
 
-    fun onBind(item: MemberPostItem) {
+    fun onBind(item: MemberPostItem, clipFuncItem: ClipFuncItem, pos: Int) {
         ibReplay.visibility = View.GONE
 
         tvTitle.text = item.title
@@ -28,5 +30,18 @@ class ClipViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         tvFavorite.text = item.favoriteCount.toString()
         tvLike.text = item.likeCount.toString()
         tvComment.text = item.commentCount.toString()
+
+        val likeRes = if (item.likeType == LikeType.LIKE) { R.drawable.ico_nice_forvideo_s } else { R.drawable.ico_nice_forvideo }
+        tvLike.setCompoundDrawablesRelativeWithIntrinsicBounds(0, likeRes, 0, 0)
+        val isLike= item.likeType == LikeType.LIKE
+        tvLike.setOnClickListener { clipFuncItem.onLikeClick(item, pos, !isLike) }
+
+        val favoriteRes = if (item.isFavorite) R.drawable.btn_favorite_forvideo_s else R.drawable.btn_favorite_forvideo_n
+        tvFavorite.setCompoundDrawablesRelativeWithIntrinsicBounds(0, favoriteRes, 0, 0)
+        tvFavorite.setOnClickListener { clipFuncItem.onFavoriteClick(item, pos, !item.isFavorite) }
+
+        ibBack.setOnClickListener { clipFuncItem.onBackClick() }
+
+
     }
 }
