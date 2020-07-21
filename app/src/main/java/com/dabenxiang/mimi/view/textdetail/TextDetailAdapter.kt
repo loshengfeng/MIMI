8fbf05490ad3a780c6b7fff9557e4e4ba5352f38
Dave Chao <dave.chao@silkrode.com.tw>
2020-07-21 11:22:42 +0800

    apply TextDetail

---
+++ app/src/main/java/com/dabenxiang/mimi/view/textdetail/TextDetailAdapter.kt
@@ -0,0 +1,215 @@
+package com.dabenxiang.mimi.view.textdetail
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Bitmap
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.dabenxiang.mimi.R
+import com.dabenxiang.mimi.callback.OnItemClickListener
+import com.dabenxiang.mimi.model.api.vo.MemberPostItem
+import com.dabenxiang.mimi.model.api.vo.MembersPostCommentItem
+import com.dabenxiang.mimi.model.api.vo.TextContentItem
+import com.dabenxiang.mimi.model.enums.CommentType
+import com.dabenxiang.mimi.model.enums.CommentViewType
+import com.dabenxiang.mimi.view.picturedetail.viewholder.CommentContentViewHolder
+import com.dabenxiang.mimi.view.picturedetail.viewholder.CommentTitleViewHolder
+import com.dabenxiang.mimi.view.player.CommentAdapter
+import com.dabenxiang.mimi.view.player.CommentLoadMoreView
+import com.dabenxiang.mimi.view.player.RootCommentNode
+import com.dabenxiang.mimi.view.textdetail.viewholder.TextDetailViewHolder
+import com.dabenxiang.mimi.widget.utility.GeneralUtils
+import com.dabenxiang.mimi.widget.utility.LruCacheUtils
+import com.google.android.material.chip.Chip
+import com.google.gson.Gson
+import java.util.*
+
+class TextDetailAdapter(
+    val context: Context,
+    private val memberPostItem: MemberPostItem,
+    private val onTextDetailListener: OnTextDetailListener,
+    private val onItemClickListener: OnItemClickListener
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        const val VIEW_TYPE_TEXT_DETAIL = 0
+        const val VIEW_TYPE_COMMENT_TITLE = 1
+        const val VIEW_TYPE_COMMENT_DATA = 2
+    }
+
+    private var commentAdapter: CommentAdapter? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val mView: View
+
+        val holder = when (viewType) {
+            VIEW_TYPE_TEXT_DETAIL -> {
+                mView = LayoutInflater.from(context)
+                    .inflate(R.layout.item_text_detail, parent, false)
+                TextDetailViewHolder(mView)
+            }
+            VIEW_TYPE_COMMENT_TITLE -> {
+                mView = LayoutInflater.from(context)
+                    .inflate(R.layout.item_comment_title, parent, false)
+                CommentTitleViewHolder(mView)
+            }
+            else -> {
+                mView = LayoutInflater.from(context)
+                    .inflate(R.layout.item_comment_content, parent, false)
+                CommentContentViewHolder(mView)
+            }
+        }
+
+        mView.setOnClickListener {
+            onItemClickListener.onItemClick()
+        }
+
+        return holder
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            0 -> VIEW_TYPE_TEXT_DETAIL
+            1 -> VIEW_TYPE_COMMENT_TITLE
+            else -> VIEW_TYPE_COMMENT_DATA
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is TextDetailViewHolder -> {
+                val contentItem =
+                    Gson().fromJson(memberPostItem.content, TextContentItem::class.java)
+                holder.posterName.text = memberPostItem.postFriendlyName
+                holder.posterTime.text = GeneralUtils.getTimeDiff(
+                    memberPostItem.creationDate, Date()
+                )
+                holder.title.text = memberPostItem.title
+                holder.desc.text = contentItem.text
+
+                val bitmap = LruCacheUtils.getLruCache(memberPostItem.avatarAttachmentId.toString())
+                Glide.with(context)
+                    .load(bitmap)
+                    .circleCrop()
+                    .into(holder.avatarImg)
+
+                val isFollow = memberPostItem.isFollow
+                if (isFollow) {
+                    holder.follow.text = context.getString(R.string.followed)
+                    holder.follow.background =
+                        context.getDrawable(R.drawable.bg_white_1_stroke_radius_16)
+                    holder.follow.setTextColor(context.getColor(R.color.color_white_1))
+                } else {
+                    holder.follow.text = context.getString(R.string.follow)
+                    holder.follow.background =
+                        context.getDrawable(R.drawable.bg_red_1_stroke_radius_16)
+                    holder.follow.setTextColor(context.getColor(R.color.color_red_1))
+                }
+
+                holder.tagChipGroup.removeAllViews()
+                memberPostItem.tags?.forEach {
+                    val chip = LayoutInflater.from(holder.tagChipGroup.context)
+                        .inflate(R.layout.chip_item, holder.tagChipGroup, false) as Chip
+                    chip.text = it
+                    chip.setTextColor(context.getColor(R.color.color_white_1_50))
+                    chip.chipBackgroundColor = ColorStateList.valueOf(
+                        ContextCompat.getColor(context, R.color.adult_color_status_bar)
+                    )
+                    holder.tagChipGroup.addView(chip)
+                }
+
+                holder.follow.setOnClickListener {
+                    onTextDetailListener.onFollowClick(memberPostItem, position, !isFollow)
+                }
+            }
+            is CommentTitleViewHolder -> {
+                holder.newestComment.setOnClickListener {
+                    holder.newestComment.setTextColor(context.getColor(R.color.color_red_1))
+                    holder.topComment.setTextColor(context.getColor(R.color.color_white_1_30))
+                    updateCommandItem(CommentType.NEWEST)
+                }
+
+                holder.topComment.setOnClickListener {
+                    holder.topComment.setTextColor(context.getColor(R.color.color_red_1))
+                    holder.newestComment.setTextColor(context.getColor(R.color.color_white_1_30))
+                    updateCommandItem(CommentType.TOP)
+                }
+            }
+            is CommentContentViewHolder -> {
+                commentAdapter = CommentAdapter(
+                    true,
+                    playerInfoListener,
+                    CommentViewType.COMMON
+                ).apply {
+                    loadMoreModule.apply {
+                        isEnableLoadMore = true
+                        isAutoLoadMore = true
+                        isEnableLoadMoreIfNotFullPage = false
+                        loadMoreView = CommentLoadMoreView(true, CommentViewType.COMMON)
+                    }
+                }
+
+                holder.commentRecycler.adapter = commentAdapter
+                updateCommandItem(CommentType.NEWEST)
+            }
+        }
+    }
+
+    private fun updateCommandItem(type: CommentType) {
+        onTextDetailListener.onGetCommandInfo(commentAdapter!!, type)
+    }
+
+    private val playerInfoListener = object : CommentAdapter.PlayerInfoListener {
+        override fun sendComment(replyId: Long?, replyName: String?) {
+            onTextDetailListener.onReplyComment(replyId, replyName)
+        }
+
+        override fun expandReply(parentNode: RootCommentNode, succeededBlock: () -> Unit) {
+            onTextDetailListener.onGetReplyCommand(parentNode, succeededBlock)
+        }
+
+        override fun replyComment(replyId: Long?, replyName: String?) {
+            onTextDetailListener.onReplyComment(replyId, replyName)
+        }
+
+        override fun setCommentLikeType(
+            replyId: Long?,
+            isLike: Boolean,
+            succeededBlock: () -> Unit
+        ) {
+            onTextDetailListener.onCommandLike(replyId, isLike, succeededBlock)
+        }
+
+        override fun removeCommentLikeType(replyId: Long?, succeededBlock: () -> Unit) {
+            onTextDetailListener.onCommandDislike(replyId, succeededBlock)
+        }
+
+        override fun getBitmap(id: Long, succeededBlock: (Bitmap) -> Unit) {
+            onTextDetailListener.onGetCommandAvatar(id, succeededBlock)
+        }
+
+        override fun onMoreClick(item: MembersPostCommentItem) {
+            onTextDetailListener.onMoreClick(item)
+        }
+    }
+
+    interface OnTextDetailListener {
+        fun onGetAttachment(id: String, position: Int)
+        fun onFollowClick(item: MemberPostItem, position: Int, isFollow: Boolean)
+        fun onGetCommandInfo(adapter: CommentAdapter, type: CommentType)
+        fun onGetReplyCommand(parentNode: RootCommentNode, succeededBlock: () -> Unit)
+        fun onCommandLike(commentId: Long?, isLike: Boolean, succeededBlock: () -> Unit)
+        fun onCommandDislike(commentId: Long?, succeededBlock: () -> Unit)
+        fun onGetCommandAvatar(id: Long, succeededBlock: (Bitmap) -> Unit)
+        fun onReplyComment(replyId: Long?, replyName: String?)
+        fun onMoreClick(item: MembersPostCommentItem)
+    }
+}
--- app/src/main/java/com/dabenxiang/mimi/view/textdetail/TextDetailFragment.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/textdetail/TextDetailFragment.kt
@@ -1,16 +1,25 @@
 package com.dabenxiang.mimi.view.textdetail
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
 import androidx.activity.addCallback
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dabenxiang.mimi.R
+import com.dabenxiang.mimi.callback.OnItemClickListener
 import com.dabenxiang.mimi.model.api.vo.MemberPostItem
+import com.dabenxiang.mimi.model.api.vo.MembersPostCommentItem
+import com.dabenxiang.mimi.model.enums.CommentType
+import com.dabenxiang.mimi.model.enums.LikeType
 import com.dabenxiang.mimi.view.base.BaseFragment
 import com.dabenxiang.mimi.view.base.NavigateItem
 import com.dabenxiang.mimi.view.picturedetail.PictureDetailFragment
-import kotlinx.android.synthetic.main.fragment_picture_detail.*
+import com.dabenxiang.mimi.view.player.CommentAdapter
+import com.dabenxiang.mimi.view.player.RootCommentNode
+import com.dabenxiang.mimi.widget.utility.GeneralUtils
+import kotlinx.android.synthetic.main.fragment_text_detail.*
 import kotlinx.android.synthetic.main.toolbar.*
 import kotlinx.android.synthetic.main.toolbar.view.*
 
@@ -31,6 +40,8 @@ class TextDetailFragment : BaseFragment() {
 
     private val viewModel: TextDetailViewModel by viewModels()
 
+    private var textDetailAdapter: TextDetailAdapter? = null
+
     override val bottomNavigationVisibility: Int
         get() = View.GONE
 
@@ -50,6 +61,25 @@ class TextDetailFragment : BaseFragment() {
             findNavController().navigateUp()
         }
 
+        textDetailAdapter = TextDetailAdapter(
+            requireContext(),
+            memberPostItem!!,
+            onTextDetailListener,
+            onItemClickListener
+        )
+
+        recycler_text_detail.layoutManager = LinearLayoutManager(context)
+        recycler_text_detail.adapter = textDetailAdapter
+        recycler_text_detail.scrollToPosition(position)
+
+        if (memberPostItem!!.likeType == LikeType.LIKE) {
+            iv_like.setImageResource(R.drawable.ico_nice_s)
+        } else {
+            iv_like.setImageResource(R.drawable.ico_nice)
+        }
+
+        tv_like_count.text = memberPostItem!!.likeCount.toString()
+        tv_comment_count.text = memberPostItem!!.commentCount.toString()
     }
 
     override fun getLayoutId(): Int {
@@ -64,5 +94,50 @@ class TextDetailFragment : BaseFragment() {
 
     }
 
+    private val onTextDetailListener = object : TextDetailAdapter.OnTextDetailListener {
+        override fun onGetAttachment(id: String, position: Int) {
 
+        }
+
+        override fun onFollowClick(item: MemberPostItem, position: Int, isFollow: Boolean) {
+
+        }
+
+        override fun onGetCommandInfo(adapter: CommentAdapter, type: CommentType) {
+
+        }
+
+        override fun onGetReplyCommand(parentNode: RootCommentNode, succeededBlock: () -> Unit) {
+
+        }
+
+        override fun onCommandLike(commentId: Long?, isLike: Boolean, succeededBlock: () -> Unit) {
+
+        }
+
+        override fun onCommandDislike(commentId: Long?, succeededBlock: () -> Unit) {
+
+        }
+
+        override fun onGetCommandAvatar(id: Long, succeededBlock: (Bitmap) -> Unit) {
+
+        }
+
+        override fun onReplyComment(replyId: Long?, replyName: String?) {
+
+        }
+
+        override fun onMoreClick(item: MembersPostCommentItem) {
+
+        }
+    }
+
+    private val onItemClickListener = object : OnItemClickListener {
+        override fun onItemClick() {
+            GeneralUtils.hideKeyboard(requireActivity())
+            layout_bar.visibility = View.VISIBLE
+            layout_edit_bar.visibility = View.INVISIBLE
+            et_message.setText("")
+        }
+    }
 }
