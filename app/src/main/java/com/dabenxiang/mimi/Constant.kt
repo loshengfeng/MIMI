35ec0e68087fd68b0e8a527175014d1d568715d1
Dave Chao <dave.chao@silkrode.com.tw>
2020-07-27 10:54:26 +0800

    added log

--- app/src/main/java/com/dabenxiang/mimi/Constant.kt
+++ app/src/main/java/com/dabenxiang/mimi/Constant.kt
@@ -3,3 +3,4 @@ package com.dabenxiang.mimi
 const val API_HOST_URL = BuildConfig.API_HOST
 const val PREFS_NAME = "MiMi_Prefs"
 const val FLURRY_API_KEY = "6G4JFXKVMW6TG9F4G5KP"
+const val PROJECT_NAME = BuildConfig.FLAVOR + "_mimi"
