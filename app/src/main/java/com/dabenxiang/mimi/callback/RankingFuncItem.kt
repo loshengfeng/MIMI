3650437b6188d418d0861e635eee065a1ae3a3ec
Joshua Liu <Joshua.liu@silkrode.com.tw>
2020-08-07 17:48:21 +0800

    Modified issues of ranking page

--- app/src/main/java/com/dabenxiang/mimi/callback/RankingFuncItem.kt
+++ app/src/main/java/com/dabenxiang/mimi/callback/RankingFuncItem.kt
@@ -4,7 +4,7 @@ import com.dabenxiang.mimi.model.api.vo.PostStatisticsItem
 import com.dabenxiang.mimi.model.api.vo.StatisticsItem
 
 class RankingFuncItem (
-    val onItemClick: (PostStatisticsItem) -> Unit = { _ -> },
+    val onItemClick: (MutableList<PostStatisticsItem>, Int) -> Unit = { _, _ -> },
     val onVideoItemClick: (StatisticsItem) -> Unit = { _ -> },
     val getBitmap: ((String, Int) -> Unit) = { _, _ -> }
 )
