cf9671e984c07649b2228e75b7414fc8828bf147
AbbieLin <abbie0216@hotmail.com>
2020-08-13 16:45:03 +0800

    fix avatarAttachment

--- app/src/main/java/com/dabenxiang/mimi/view/favroite/FavoritePostViewHolder.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/favroite/FavoritePostViewHolder.kt
@@ -7,12 +7,6 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.Priority
-import com.bumptech.glide.load.MultiTransformation
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.CircleCrop
-import com.bumptech.glide.request.RequestOptions
-import com.dabenxiang.mimi.App
 import com.dabenxiang.mimi.R
 import com.dabenxiang.mimi.model.api.vo.MediaContentItem
 import com.dabenxiang.mimi.model.api.vo.PostFavoriteItem
@@ -105,24 +99,19 @@ class FavoritePostViewHolder(
     override fun updated(position: Int) {
         val isMe = data?.posterId == accountManager.getProfile().userId
         data?.position = position
-        data?.posterAvatarAttachmentId?.let {
-            LruCacheUtils.getLruCache(it.toString())?.also { bitmap ->
-                val options: RequestOptions = RequestOptions()
-                    .transform(MultiTransformation(CenterCrop(), CircleCrop()))
-                    .placeholder(R.drawable.default_profile_picture)
-                    .error(R.drawable.default_profile_picture)
-                    .priority(Priority.NORMAL)
-
-                Glide.with(App.self).load(bitmap)
-                    .apply(options)
-                    .into(ivHead)
+        data?.posterAvatarAttachmentId
+            .takeIf { it != null && it.toString() != LruCacheUtils.ZERO_ID }?.also { id ->
+                LruCacheUtils.getLruCache(id.toString())?.also { bitmap ->
+                    Glide.with(ivHead.context).load(bitmap).circleCrop().into(ivHead)
+                } ?: run {
+                    listener.onGetAttachment(
+                        data!!.posterAvatarAttachmentId!!.toString(),
+                        position,
+                        AttachmentType.ADULT_AVATAR
+                    )
+                }
             } ?: run {
-                listener.onGetAttachment(
-                    it.toString(),
-                    position,
-                    AttachmentType.ADULT_AVATAR
-                )
-            }
+            Glide.with(ivHead.context).load(R.drawable.default_profile_picture).circleCrop().into(ivHead)
         }
 
         tvName.text = data?.posterName
