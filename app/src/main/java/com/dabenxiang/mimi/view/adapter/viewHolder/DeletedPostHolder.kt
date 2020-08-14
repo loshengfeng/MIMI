dce56b6091c043df55e0e628232580a5498952fc
AbbieLin <abbie0216@hotmail.com>
2020-08-14 15:10:26 +0800

    fix delete behavior

---
+++ app/src/main/java/com/dabenxiang/mimi/view/adapter/viewHolder/DeletedPostHolder.kt
@@ -0,0 +1,6 @@
+package com.dabenxiang.mimi.view.adapter.viewHolder
+
+import android.view.View
+import com.dabenxiang.mimi.view.base.BaseViewHolder
+
+class DeletedPostHolder(itemView: View) : BaseViewHolder(itemView)
