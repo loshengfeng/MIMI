2c63cf168e590a39ad100a6063983990396337a9
Dave Chao <dave.chao@silkrode.com.tw>
2020-08-18 15:38:53 +0800

    added order result ui

--- app/src/main/java/com/dabenxiang/mimi/view/orderresult/OrderResultFragment.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/orderresult/OrderResultFragment.kt
@@ -2,19 +2,41 @@ package com.dabenxiang.mimi.view.orderresult
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.addCallback
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dabenxiang.mimi.R
 import com.dabenxiang.mimi.view.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_order_result.*
 
 class OrderResultFragment : BaseFragment() {
 
     private val viewModel: OrderResultViewModel by viewModels()
+
+    private val controller = OrderResultEpoxyController()
 
     override val bottomNavigationVisibility: Int
         get() = View.GONE
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().onBackPressedDispatcher.addCallback { }
+
+        tv_step1.background = ContextCompat.getDrawable(
+            requireContext(), R.drawable.bg_blue_1_oval
+        )
+
+        tv_step2.background = ContextCompat.getDrawable(
+            requireContext(), R.drawable.bg_black_1_oval
+        )
+
+        line.setBackgroundColor(requireContext().getColor(R.color.color_black_1))
+        tv_create_order.setTextColor(requireContext().getColor(R.color.color_black_1))
+
+        recycler_order_result.layoutManager = LinearLayoutManager(requireContext())
+        recycler_order_result.adapter = controller.adapter
     }
 
     override fun getLayoutId(): Int {
@@ -28,5 +50,4 @@ class OrderResultFragment : BaseFragment() {
     override fun setupListeners() {
 
     }
-
 }
