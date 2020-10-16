7c5a10d530b3e338437e967131cbcc4840c5cf9c
patrick.lee <patrick@patrickleede-MacBook-Pro.local>
2020-10-16 14:37:37 +0800

    add inviteVipFragment and button

---
+++ app/src/main/java/com/dabenxiang/mimi/view/invitevip/InviteVipFragment.kt
@@ -0,0 +1,18 @@
+package com.dabenxiang.mimi.view.invitevip
+
+import com.dabenxiang.mimi.R
+import com.dabenxiang.mimi.view.base.BaseFragment
+
+class InviteVipFragment : BaseFragment() {
+    override fun getLayoutId(): Int {
+        return R.layout.fragment_invite_vip
+    }
+
+    override fun setupObservers() {
+
+    }
+
+    override fun setupListeners() {
+
+    }
+}
---
+++ app/src/main/java/com/dabenxiang/mimi/view/invitevip/InviteVipViewModel.kt
@@ -0,0 +1,7 @@
+package com.dabenxiang.mimi.view.invitevip
+
+import com.dabenxiang.mimi.view.base.BaseViewModel
+
+class InviteVipViewModel : BaseViewModel() {
+
+}
