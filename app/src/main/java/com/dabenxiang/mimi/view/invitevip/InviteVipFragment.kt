37eeff5d664ad0abaf8f886082f9f9276608582b
patrick.lee <patrick@patrickleede-MacBook-Pro.local>
2020-10-16 17:44:20 +0800

    finish fragment_invite_vip layout

--- app/src/main/java/com/dabenxiang/mimi/view/invitevip/InviteVipFragment.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/invitevip/InviteVipFragment.kt
@@ -1,9 +1,13 @@
 package com.dabenxiang.mimi.view.invitevip
 
+import android.view.View
 import com.dabenxiang.mimi.R
 import com.dabenxiang.mimi.view.base.BaseFragment
 
 class InviteVipFragment : BaseFragment() {
+    override val bottomNavigationVisibility: Int
+        get() = View.GONE
+
     override fun getLayoutId(): Int {
         return R.layout.fragment_invite_vip
     }
