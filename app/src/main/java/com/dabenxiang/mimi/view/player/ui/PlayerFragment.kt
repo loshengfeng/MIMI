9b1893f96eb2f2385bdac33c8d4661c6825d4540
Arvin <arvin.luo@silkrode.com.tw>
2020-11-05 18:12:02 +0800

    [ Fix ] Status black color

--- app/src/main/java/com/dabenxiang/mimi/view/player/ui/PlayerFragment.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/player/ui/PlayerFragment.kt
@@ -241,7 +241,7 @@ class PlayerFragment : BaseFragment() {
     override fun setUpStatusBarColor() {
         activity?.also {
             (it as MainActivity).window.statusBarColor =
-                requireContext().getColor(R.color.adult_color_status_bar)
+                requireContext().getColor(R.color.color_black_1)
         }
     }
 
