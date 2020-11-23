f43ca1daebfb1778ea1584d78e9c7fca6e76d689
Arvin <arvin.luo@silkrode.com.tw>
2020-11-23 16:54:00 +0800

    [ Feature ] Hide navigation and set back event

--- app/src/main/java/com/dabenxiang/mimi/view/club/ClubTextDetailFragment.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/club/ClubTextDetailFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.View
 import com.dabenxiang.mimi.R
 import com.dabenxiang.mimi.view.base.BaseFragment
+import com.dabenxiang.mimi.view.base.NavigateItem
 import kotlinx.android.synthetic.main.fragment_order.*
 import kotlinx.android.synthetic.main.item_setting_bar.*
 
 class ClubTextDetailFragment : BaseFragment() {
+
+    override val bottomNavigationVisibility: Int
+        get() = View.GONE
 
     override fun getLayoutId() = R.layout.fragment_club_text_detail
 
@@ -15,6 +19,9 @@ class ClubTextDetailFragment : BaseFragment() {
     }
 
     override fun setupListeners() {
+        tv_back.setOnClickListener {
+            navigateTo(NavigateItem.Up)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
