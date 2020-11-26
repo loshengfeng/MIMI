edaca5b96688402ec4164ec1207e5cd4ed0cc7b9
neo <neo.chang@silkrode.com.tw>
2020-11-26 17:27:36 +0800

    modify short

--- app/src/main/java/com/dabenxiang/mimi/view/club/short/ClubShortVideoFragment.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/club/short/ClubShortVideoFragment.kt
@@ -3,33 +3,55 @@ package com.dabenxiang.mimi.view.club.post
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.dabenxiang.mimi.R
 import com.dabenxiang.mimi.callback.AdultListener
+import com.dabenxiang.mimi.callback.AttachmentListener
 import com.dabenxiang.mimi.callback.MemberPostFuncItem
+import com.dabenxiang.mimi.callback.MyPostListener
+import com.dabenxiang.mimi.model.api.ApiResult
 import com.dabenxiang.mimi.model.api.vo.MemberPostItem
 import com.dabenxiang.mimi.model.enums.AdultTabType
+import com.dabenxiang.mimi.model.enums.AttachmentType
+import com.dabenxiang.mimi.model.enums.LoadImageType
 import com.dabenxiang.mimi.model.enums.PostType
+import com.dabenxiang.mimi.model.vo.SearchPostItem
 import com.dabenxiang.mimi.view.adapter.MemberPostPagedAdapter
 import com.dabenxiang.mimi.view.base.BaseFragment
 import com.dabenxiang.mimi.view.base.NavigateItem
 import com.dabenxiang.mimi.view.clip.ClipFragment
+import com.dabenxiang.mimi.view.club.recommend.ClubRecommendAdapter
+import com.dabenxiang.mimi.view.club.recommend.ClubRecommendViewModel
 import com.dabenxiang.mimi.view.mypost.MyPostFragment
 import com.dabenxiang.mimi.view.picturedetail.PictureDetailFragment
 import com.dabenxiang.mimi.view.post.BasePostFragment
+import com.dabenxiang.mimi.view.search.post.SearchPostFragment
+import com.dabenxiang.mimi.view.textdetail.TextDetailFragment
+import com.dabenxiang.mimi.widget.utility.GeneralUtils
 import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.fragment_club_latest.*
+import kotlinx.android.synthetic.main.fragment_club_latest.id_empty_group
+import kotlinx.android.synthetic.main.fragment_club_latest.layout_ad
+import kotlinx.android.synthetic.main.fragment_club_latest.layout_refresh
+import kotlinx.android.synthetic.main.fragment_club_latest.recycler_view
 import kotlinx.android.synthetic.main.fragment_club_post_text.*
+import kotlinx.android.synthetic.main.fragment_club_short.*
 import kotlinx.android.synthetic.main.fragment_club_text.*
 import kotlinx.android.synthetic.main.fragment_order.*
 import kotlinx.android.synthetic.main.fragment_order.viewPager
+import kotlinx.android.synthetic.main.item_ad.view.*
 import kotlinx.android.synthetic.main.item_setting_bar.*
 import timber.log.Timber
 
 class ClubShortVideoFragment : BaseFragment() {
-
+    private val viewModel: ClubShortVideoViewModel by viewModels()
+    private var adapter: ClubShortVideoAdapter? = null
+    override fun getLayoutId() = R.layout.fragment_club_short
     override val bottomNavigationVisibility: Int
         get() = View.GONE
 
@@ -44,20 +66,242 @@ class ClubShortVideoFragment : BaseFragment() {
         }
     }
 
-    override fun getLayoutId() = R.layout.fragment_club_text
+    private val memberPostFuncItem by lazy {
+        MemberPostFuncItem(
+            {},
+            { id, view, type -> },
+            { item, items, isFollow, func -> followMember(item, items, isFollow, func) },
+            { item, isLike, func -> },
+            { item, isFavorite, func -> }
+        )
+    }
+
+    private val attachmentListener = object : AttachmentListener {
+        override fun onGetAttachment(id: Long?, view: ImageView, type: LoadImageType) {
+            viewModel.loadImage(id, view, type)
+        }
+
+        override fun onGetAttachment(id: String, parentPosition: Int, position: Int) {
+        }
+    }
+
+    private val postListener = object : MyPostListener {
+        override fun onMoreClick(item: MemberPostItem) {
+            onMoreClick(item, ArrayList(adapter?.currentList as List<MemberPostItem>), onEdit = {
+                it as MemberPostItem
+            })
+        }
+
+        override fun onLikeClick(item: MemberPostItem, position: Int, isLike: Boolean) {
+//            checkStatus { viewModel.likePost(item, position, isLike) }
+        }
+
+        override fun onClipCommentClick(item: List<MemberPostItem>, position: Int) {
+            checkStatus {
+                val bundle = ClipFragment.createBundle(ArrayList(mutableListOf(item[position])), 0)
+//                navigationToVideo(bundle)
+            }
+        }
+
+        override fun onClipItemClick(item: List<MemberPostItem>, position: Int) {
+            val bundle = ClipFragment.createBundle(ArrayList(mutableListOf(item[position])), 0)
+//            navigationToVideo(bundle)
+        }
+
+        override fun onChipClick(type: PostType, tag: String) {
+            val item = SearchPostItem(type, tag)
+            val bundle = SearchPostFragment.createBundle(item)
+            navigateTo(
+                NavigateItem.Destination(
+                    R.id.action_clubTabFragment_to_searchPostFragment,
+                    bundle
+                )
+            )
+        }
+
+        override fun onItemClick(item: MemberPostItem, adultTabType: AdultTabType) {
+            when (adultTabType) {
+                AdultTabType.PICTURE -> {
+                    val bundle = PictureDetailFragment.createBundle(item, 0)
+                    navigateTo(
+                        NavigateItem.Destination(
+                            R.id.action_clubTabFragment_to_clubPicDetailFragment,
+                            bundle
+                        )
+                    )
+                }
+                AdultTabType.TEXT -> {
+                    val bundle = TextDetailFragment.createBundle(item, 0)
+                    navigateTo(
+                        NavigateItem.Destination(
+                            R.id.action_clubTabFragment_to_clubTextDetailFragment,
+                            bundle
+                        )
+                    )
+                }
+                AdultTabType.CLIP -> {
+                    //todo 跳轉到短視頻內頁
+                }
+            }
+        }
+
+        override fun onCommentClick(item: MemberPostItem, adultTabType: AdultTabType) {
+            checkStatus {
+                when (adultTabType) {
+                    AdultTabType.PICTURE -> {
+                        val bundle = PictureDetailFragment.createBundle(item, 1)
+//                        navigationToPicture(bundle)
+                    }
+                    AdultTabType.TEXT -> {
+                        val bundle = TextDetailFragment.createBundle(item, 1)
+//                        navigationToText(bundle)
+                    }
+                }
+            }
+        }
+
+        override fun onFavoriteClick(
+            item: MemberPostItem,
+            position: Int,
+            isFavorite: Boolean,
+            type: AttachmentType
+        ) {
+            checkStatus {
+                viewModel.favoritePost(item, position, isFavorite)
+            }
+        }
+
+        override fun onFollowClick(
+            items: List<MemberPostItem>,
+            position: Int,
+            isFollow: Boolean
+        ) {
+            checkStatus { viewModel.followPost(ArrayList(items), position, isFollow) }
+        }
+    }
 
     override fun setupObservers() {
+        viewModel.adResult.observe(this, {
+            when (it) {
+                is ApiResult.Success -> {
+                    it.result?.let { item ->
+                        Glide.with(requireContext()).load(item.href).into(layout_ad.iv_ad)
+                        layout_ad.iv_ad.setOnClickListener {
+                            GeneralUtils.openWebView(requireContext(), item.target ?: "")
+                        }
+                    }
+                }
+                is ApiResult.Error -> {
+                    layout_ad.visibility =View.GONE
+                    onApiError(it.throwable)
+                }
 
+                else -> {
+                    layout_ad.visibility =View.GONE
+                    onApiError(Exception("Unknown Error!"))
+                }
+            }
+        })
+
+        viewModel.showProgress.observe(this, {
+            layout_refresh.isRefreshing = it
+        })
+
+        viewModel.clubCount.observe(this, Observer {
+            if (it <= 0) {
+                id_empty_group.visibility = View.VISIBLE
+                recycler_view.visibility = View.INVISIBLE
+            } else {
+                id_empty_group.visibility = View.GONE
+                recycler_view.visibility = View.VISIBLE
+            }
+            layout_refresh.isRefreshing = false
+        })
+
+        viewModel.postItemListResult.observe(viewLifecycleOwner, Observer {
+            adapter?.submitList(it)
+        })
+
+        viewModel.followResult.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is ApiResult.Empty -> {
+                    adapter?.notifyItemRangeChanged(
+                        0,
+                        viewModel.totalCount,
+                        ClubRecommendAdapter.PAYLOAD_UPDATE_FOLLOW
+                    )
+                }
+                is ApiResult.Error -> onApiError(it.throwable)
+            }
+        })
+
+        viewModel.likePostResult.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is ApiResult.Success -> {
+                    adapter?.notifyItemChanged(
+                        it.result,
+                        ClubRecommendAdapter.PAYLOAD_UPDATE_LIKE
+                    )
+                }
+                is ApiResult.Error -> Timber.e(it.throwable)
+            }
+        })
+
+        viewModel.favoriteResult.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is ApiResult.Success -> {
+                    adapter?.notifyItemChanged(
+                        it.result,
+                        ClubRecommendAdapter.PAYLOAD_UPDATE_FAVORITE
+                    )
+                }
+                is ApiResult.Error -> onApiError(it.throwable)
+            }
+        })
     }
 
     override fun setupListeners() {
-        tv_back.setOnClickListener {
-            navigateTo(NavigateItem.Up)
-        }
+//        tv_back.setOnClickListener {
+//            navigateTo(NavigateItem.Up)
+//        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+//        viewModel.adWidth = ((GeneralUtils.getScreenSize(requireActivity()).first) * 0.333).toInt()
+//        viewModel.adHeight = (viewModel.adWidth * 0.142).toInt()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
 
+    override fun initSettings() {
+        layout_refresh.setOnRefreshListener {
+            layout_refresh.isRefreshing = false
+            getData()
+        }
+        adapter = ClubShortVideoAdapter(requireContext(),
+            false,
+            postListener,
+            attachmentListener,
+            memberPostFuncItem)
+        list_short.adapter = adapter
+    }
+
+    private fun getData(){
+        viewModel.getAd()
+        viewModel.getPostItemList()
+    }
+
+    private fun followMember(
+        memberPostItem: MemberPostItem,
+        items: List<MemberPostItem>,
+        isFollow: Boolean,
+        update: (Boolean) -> Unit
+    ) {
+        checkStatus {
+            viewModel.followMember(memberPostItem, ArrayList(items), isFollow, update)
+        }
     }
 }
--- app/src/main/java/com/dabenxiang/mimi/view/club/short/ClubShortVideoViewModel.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/club/short/ClubShortVideoViewModel.kt
@@ -4,63 +4,93 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.asFlow
 import androidx.lifecycle.viewModelScope
+import androidx.paging.DataSource
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
+import com.dabenxiang.mimi.callback.MyFollowPagingCallback
 import com.dabenxiang.mimi.callback.PagingCallback
+import com.dabenxiang.mimi.model.api.ApiResult
+import com.dabenxiang.mimi.model.api.vo.AdItem
+import com.dabenxiang.mimi.model.api.vo.LikeRequest
 import com.dabenxiang.mimi.model.api.vo.MemberPostItem
 import com.dabenxiang.mimi.model.enums.CategoryType
+import com.dabenxiang.mimi.model.enums.LikeType
 import com.dabenxiang.mimi.model.enums.PostType
 import com.dabenxiang.mimi.view.base.BaseViewModel
+import com.dabenxiang.mimi.view.club.recommend.ClubRecommendListDataSource
 import com.dabenxiang.mimi.view.home.memberpost.MemberPostDataSource
 import com.dabenxiang.mimi.view.home.memberpost.MemberPostFactory
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import timber.log.Timber
 
 class ClubShortVideoViewModel : BaseViewModel() {
 
-    private val _textPostItemListResult = MutableLiveData<PagedList<MemberPostItem>>()
-    val textPostItemListResult: LiveData<PagedList<MemberPostItem>> = _textPostItemListResult
+    private val _adResult = MutableLiveData<ApiResult<AdItem>>()
+    val adResult: LiveData<ApiResult<AdItem>> = _adResult
 
-    private val _totalCountResult = MutableLiveData<Pair<CategoryType,Int>>()
-    val totalCountResult: LiveData<Pair<CategoryType,Int>> = _totalCountResult
+    private val _clubCount = MutableLiveData<Int>()
+    val clubCount: LiveData<Int> = _clubCount
 
-    private val _picturePostItemListResult = MutableLiveData<PagedList<MemberPostItem>>()
-    val picturePostItemListResult: LiveData<PagedList<MemberPostItem>> = _picturePostItemListResult
+    private val _postItemListResult = MutableLiveData<PagedList<MemberPostItem>>()
+    val postItemListResult: LiveData<PagedList<MemberPostItem>> = _postItemListResult
+
+    private val _followResult = MutableLiveData<ApiResult<Nothing>>()
+    val followResult: LiveData<ApiResult<Nothing>> = _followResult
+
+    private var _likePostResult = MutableLiveData<ApiResult<Int>>()
+    val likePostResult: LiveData<ApiResult<Int>> = _likePostResult
+
+    private var _favoriteResult = MutableLiveData<ApiResult<Int>>()
+    val favoriteResult: LiveData<ApiResult<Int>> = _favoriteResult
+
+    private val _clubIdList = ArrayList<Long>()
 
     var totalCount: Int = 0
 
-    fun getTextPosts() {
+    fun getPostItemList() {
         viewModelScope.launch {
-            getMemberPostPagingItems(PostType.TEXT).asFlow()
-                .collect { _textPostItemListResult.value = it }
+            getRecommendPostPagingItems()
+                .asFlow()
+                .collect { _postItemListResult.value = it }
         }
     }
 
-    fun getPicturePosts() {
-        viewModelScope.launch {
-            getMemberPostPagingItems(PostType.IMAGE).asFlow()
-                .collect { _picturePostItemListResult.value = it }
+    private fun getRecommendPostPagingItems(): LiveData<PagedList<MemberPostItem>> {
+        val dataSourceFactory = object : DataSource.Factory<Int, MemberPostItem>() {
+            override fun create(): DataSource<Int, MemberPostItem> {
+                return ClubRecommendListDataSource(
+                    domainManager,
+                    pagingCallback,
+                    viewModelScope,
+                    adWidth,
+                    adHeight
+                )
+            }
         }
-    }
 
-    private fun getMemberPostPagingItems(postType: PostType): LiveData<PagedList<MemberPostItem>> {
-        val pictureDataSource =
-            MemberPostDataSource(
-                HomePagingCallBack(CategoryType.valueOf(postType.name)),
-                viewModelScope,
-                domainManager,
-                postType,
-                adWidth,
-                adHeight
-            )
-        val pictureFactory = MemberPostFactory(pictureDataSource)
         val config = PagedList.Config.Builder()
             .setPrefetchDistance(4)
             .build()
-        return LivePagedListBuilder(pictureFactory, config).build()
+        return LivePagedListBuilder(dataSourceFactory, config).build()
     }
 
-    inner class HomePagingCallBack(private val type: CategoryType) : PagingCallback {
+    fun getAd() {
+        viewModelScope.launch {
+            flow {
+                val adResult = domainManager.getAdRepository().getAD(adWidth, adHeight)
+                if (!adResult.isSuccessful) throw HttpException(adResult)
+                emit(ApiResult.success(adResult.body()?.content))
+            }
+                .flowOn(Dispatchers.IO)
+                .collect { _adResult.value = it}
+        }
+
+    }
+
+    private val pagingCallback = object : PagingCallback {
         override fun onLoading() {
             setShowProgress(true)
         }
@@ -70,17 +100,113 @@ class ClubShortVideoViewModel : BaseViewModel() {
         }
 
         override fun onThrowable(throwable: Throwable) {
-
-        }
-
-        override fun onTotalCount(count: Long) {
-            _totalCountResult.postValue(Pair(type, count.toInt()))
+            Timber.e(throwable)
+            _clubCount.postValue(0)
         }
 
         override fun onCurrentItemCount(count: Long, isInitial: Boolean) {
-            totalCount = if (isInitial) count.toInt()
-            else totalCount.plus(count.toInt())
-            if (isInitial) cleanRemovedPosList()
+            _clubCount.postValue(count.toInt())
+        }
+    }
+
+    private val clubPagingCallback = object : MyFollowPagingCallback {
+        override fun onTotalCount(count: Long) {
+            _clubCount.postValue(count.toInt())
+        }
+
+        override fun onIdList(list: ArrayList<Long>, isInitial: Boolean) {
+            if (isInitial) _clubIdList.clear()
+            _clubIdList.addAll(list)
+        }
+    }
+
+    fun followMember(
+        item: MemberPostItem,
+        items: ArrayList<MemberPostItem>,
+        isFollow: Boolean,
+        update: (Boolean) -> Unit
+    ) {
+        viewModelScope.launch {
+            flow {
+                val apiRepository = domainManager.getApiRepository()
+                val result = when {
+                    isFollow -> apiRepository.followPost(item.creatorId)
+                    else -> apiRepository.cancelFollowPost(item.creatorId)
+                }
+                if (!result.isSuccessful) throw HttpException(result)
+                items.forEach {
+                    if (it.creatorId == item.creatorId) {
+                        it.isFollow = isFollow
+                    }
+                }
+                emit(ApiResult.success(null))
+            }
+                .flowOn(Dispatchers.IO)
+                .onStart { emit(ApiResult.loading()) }
+                .onCompletion { emit(ApiResult.loaded()) }
+                .catch { e -> emit(ApiResult.error(e)) }
+                .collect { _followResult.value = it }
+        }
+    }
+
+    fun favoritePost(
+        item: MemberPostItem,
+        position: Int,
+        isFavorite: Boolean
+    ) {
+        viewModelScope.launch {
+            flow {
+                val apiRepository = domainManager.getApiRepository()
+                val result = when {
+                    isFavorite -> apiRepository.addFavorite(item.id)
+                    else -> apiRepository.deleteFavorite(item.id)
+                }
+                if (!result.isSuccessful) throw HttpException(result)
+                emit(ApiResult.success(position))
+            }
+                .flowOn(Dispatchers.IO)
+                .catch { e -> emit(ApiResult.error(e)) }
+                .collect { _favoriteResult.value = it }
+        }
+    }
+
+    fun likePost(item: MemberPostItem, position: Int, isLike: Boolean) {
+        viewModelScope.launch {
+            flow {
+                val apiRepository = domainManager.getApiRepository()
+                val likeType = when {
+                    isLike -> LikeType.LIKE
+                    else -> LikeType.DISLIKE
+                }
+                val request = LikeRequest(likeType)
+                val result = apiRepository.like(item.id, request)
+                if (!result.isSuccessful) throw HttpException(result)
+                emit(ApiResult.success(position))
+            }
+                .flowOn(Dispatchers.IO)
+                .catch { e -> emit(ApiResult.error(e)) }
+                .collect { _likePostResult.value = it }
+        }
+    }
+
+    fun followPost(items: ArrayList<MemberPostItem>, position: Int, isFollow: Boolean) {
+        viewModelScope.launch {
+            flow {
+                val apiRepository = domainManager.getApiRepository()
+                val result = when {
+                    isFollow -> apiRepository.followPost(items[position].creatorId)
+                    else -> apiRepository.cancelFollowPost(items[position].creatorId)
+                }
+                if (!result.isSuccessful) throw HttpException(result)
+                items.forEach { item ->
+                    if (items[position].creatorId == item.creatorId)
+                        item.isFollow = isFollow
+                }
+                emit(ApiResult.success(null))
+            }
+                .flowOn(Dispatchers.IO)
+                .catch { e -> emit(ApiResult.error(e)) }
+                .collect { _followResult.value = it }
         }
     }
 }
