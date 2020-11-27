cab2fbb9736faad8eb1e9c25d1a591bf1a4ddeee
sion <sion.wang@silkrode.com.tw>
2020-11-27 19:07:19 +0800

    Fixed paging error.

--- app/src/main/java/com/dabenxiang/mimi/view/clip/ClipPagingSource.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/clip/ClipPagingSource.kt
@@ -1,14 +1,9 @@
 package com.dabenxiang.mimi.view.clip
 
 import androidx.paging.PagingSource
-import com.dabenxiang.mimi.model.api.vo.MemberPostItem
 import com.dabenxiang.mimi.model.api.vo.VideoItem
-import com.dabenxiang.mimi.model.api.vo.VideoSearchItem
-import com.dabenxiang.mimi.model.enums.PostType
 import com.dabenxiang.mimi.model.enums.StatisticsOrderType
 import com.dabenxiang.mimi.model.manager.DomainManager
-import com.dabenxiang.mimi.model.vo.BaseVideoItem
-import com.dabenxiang.mimi.view.home.memberpost.MemberPostDataSource
 import retrofit2.HttpException
 
 class ClipPagingSource(
@@ -24,25 +19,25 @@ class ClipPagingSource(
             val offset = params.key ?: 0L
             val result = domainManager.getApiRepository().searchShortVideo(
                 orderByType = orderByType,
-                offset = 0.toString(),
+                offset = offset.toString(),
                 limit = PER_LIMIT.toString()
             )
             if (!result.isSuccessful) throw HttpException(result)
             val item = result.body()
+
             val videos = item?.content?.videos
             val nextOffset = when {
                 hasNextPage(
                     item?.paging?.count ?: 0,
                     item?.paging?.offset ?: 0,
-                    videos?.size ?: 0,
-                    params.loadSize
-                ) -> offset + params.loadSize
+                    videos?.size ?: 0
+                ) -> offset + PER_LIMIT
                 else -> null
             }
 
             LoadResult.Page(
                 data = videos ?: arrayListOf(),
-                prevKey = if (offset == 0L) null else offset - params.loadSize.toLong(),
+                prevKey = if (offset == 0L) null else offset - PER_LIMIT,
                 nextKey = nextOffset
             )
         } catch (exception: Exception) {
@@ -50,9 +45,9 @@ class ClipPagingSource(
         }
     }
 
-    private fun hasNextPage(total: Long, offset: Long, currentSize: Int, loadSize: Int): Boolean {
+    private fun hasNextPage(total: Long, offset: Long, currentSize: Int): Boolean {
         return when {
-            currentSize < loadSize -> false
+            currentSize < PER_LIMIT -> false
             offset >= total -> false
             else -> true
         }
