add4f06605020e86ffe8a523982d7be047b6939c
Neo.hu <neo.hu@silkrode.com.tw>
2020-12-02 02:03:26 +0800

    Add My Follow List Adapter

--- app/src/main/java/com/dabenxiang/mimi/view/myfollow/follow_list/MyFollowListFragment.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/myfollow/follow_list/MyFollowListFragment.kt
@@ -6,25 +6,38 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import com.dabenxiang.mimi.R
 import com.dabenxiang.mimi.model.api.ApiResult
+import com.dabenxiang.mimi.callback.BaseItemListener
+import com.dabenxiang.mimi.model.enums.ClickType
 import com.dabenxiang.mimi.view.base.BaseFragment
 import com.dabenxiang.mimi.view.myfollow.MyFollowFragment
 import com.dabenxiang.mimi.view.myfollow.MyFollowViewModel
 import com.dabenxiang.mimi.widget.utility.GeneralUtils
+import kotlinx.android.synthetic.main.fragment_my_follow_list.*
 
-class  MyFollowListFragment(val type: Int) : BaseFragment() {
+class MyFollowListFragment(val type: Int) : BaseFragment() {
     private val viewModel: MyFollowListViewModel by viewModels()
-    private val myFollowViewModel: MyFollowViewModel by viewModels({requireParentFragment()})
+    private val myFollowViewModel: MyFollowViewModel by viewModels({ requireParentFragment() })
+    private var memberAdapter: MemberFollowPeopleAdapter? = null
+    private var clubAdapter: ClubFollowPeopleAdapter? = null
+
+
     override fun getLayoutId() = R.layout.fragment_my_follow_list
+
+    private val listener: BaseItemListener = object : BaseItemListener {
+        override fun onItemClick(item: Any, type: ClickType) {
+        }
+
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         myFollowViewModel.deleteFollow.observe(this, {
             if (type == it) {
-               when(it){
-                   //TODO Need adapter
-                   MyFollowFragment.TAB_FOLLOW_PEOPLE -> viewModel.cleanAllFollowMember(listOf())
-                   MyFollowFragment.TAB_FOLLOW_CLUB -> viewModel.cleanAllFollowClub(listOf())
-               }
+                when (it) {
+                    //TODO Need adapter
+                    MyFollowFragment.TAB_FOLLOW_PEOPLE -> viewModel.cleanAllFollowMember(listOf())
+                    MyFollowFragment.TAB_FOLLOW_CLUB -> viewModel.cleanAllFollowClub(listOf())
+                }
             }
         })
 
@@ -45,14 +58,30 @@ class  MyFollowListFragment(val type: Int) : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
+        when (type) {
+            MyFollowFragment.TAB_FOLLOW_PEOPLE -> {
+                memberAdapter = MemberFollowPeopleAdapter(requireContext(), listener)
+                recycler_view.adapter = memberAdapter
+            }
+            MyFollowFragment.TAB_FOLLOW_CLUB -> {
+                clubAdapter = ClubFollowPeopleAdapter(requireContext(), listener)
+                recycler_view.adapter = clubAdapter
+            }
+        }
 
     }
 
     override fun onResume() {
         super.onResume()
+        when (type) {
+            MyFollowFragment.TAB_FOLLOW_PEOPLE -> {
+                memberAdapter?.let { viewModel.getMemberData() }
+            }
+            MyFollowFragment.TAB_FOLLOW_CLUB -> {
+                clubAdapter?.let {
+                    viewModel.getClubFollowData()
+                }
+            }
+        }
     }
-
-
 }
--- app/src/main/java/com/dabenxiang/mimi/view/myfollow/follow_list/MyFollowListViewModel.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/myfollow/follow_list/MyFollowListViewModel.kt
@@ -6,18 +6,98 @@ import androidx.lifecycle.viewModelScope
 import com.dabenxiang.mimi.model.api.ApiResult
 import com.dabenxiang.mimi.model.api.vo.ClubFollowItem
 import com.dabenxiang.mimi.model.api.vo.MemberFollowItem
-import com.dabenxiang.mimi.model.api.vo.MemberPostItem
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.dabenxiang.mimi.callback.PagingCallback
 import com.dabenxiang.mimi.view.club.base.ClubViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
-import kotlin.collections.forEach
+import com.dabenxiang.mimi.view.myfollow.ClubFollowListDataSource
+import com.dabenxiang.mimi.view.myfollow.MemberFollowListDataSource
+import com.dabenxiang.mimi.view.myfollow.MyFollowFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import timber.log.Timber
 
 class MyFollowListViewModel : ClubViewModel() {
 
     private val _cleanResult = MutableLiveData<ApiResult<Nothing>>()
     val cleanResult: LiveData<ApiResult<Nothing>> = _cleanResult
+    private val _postCount = MutableLiveData<Int>()
+
+    val postCount: LiveData<Int> = _postCount
+
+    fun getClubFollowData(adapter: ClubFollowPeopleAdapter) {
+        Timber.i("getData")
+//        when (type) {
+//            MyFollowFragment.TAB_FOLLOW_PEOPLE -> {
+                CoroutineScope(Dispatchers.IO).launch {
+                    adapter.submitData(PagingData.empty())
+                    getClubFollowList()
+                            .collectLatest {
+                                adapter.submitData(it)
+                            }
+//                }
+//            }
+
+        }
+    }
+
+
+    private fun getClubFollowList(): Flow<PagingData<ClubFollowItem>> {
+        return Pager(
+                config = PagingConfig(pageSize = ClubFollowListDataSource.PER_LIMIT),
+                pagingSourceFactory = {
+                    ClubFollowListDataSource(
+                            domainManager,
+                            pagingCallback
+                    )
+                }
+        )
+                .flow
+                .onStart { setShowProgress(true) }
+                .onCompletion { setShowProgress(false) }
+                .cachedIn(viewModelScope)
+    }
+
+    fun getMemberData(adapter: MemberFollowPeopleAdapter){
+            CoroutineScope(Dispatchers.IO).launch {
+                adapter.submitData(PagingData.empty())
+                getMemberFollowList()
+                        .collectLatest {
+                            adapter.submitData(it)
+                        }
+            }
+    }
+
+    private fun getMemberFollowList(): Flow<PagingData<MemberFollowItem>> {
+        return Pager(
+                config = PagingConfig(pageSize = MemberFollowListDataSource.PER_LIMIT),
+                pagingSourceFactory = {
+                    MemberFollowListDataSource(
+                            domainManager,
+                            pagingCallback
+                    )
+                }
+        )
+                .flow
+                .onStart { setShowProgress(true) }
+                .onCompletion { setShowProgress(false) }
+                .cachedIn(viewModelScope)
+    }
+
+    private val pagingCallback = object : PagingCallback {
+        override fun onTotalCount(count: Long) {
+            _postCount.postValue(count.toInt())
+        }
+    }
 
     fun cleanAllFollowMember(items: List<MemberFollowItem>) {
         viewModelScope.launch {
@@ -54,5 +134,4 @@ class MyFollowListViewModel : ClubViewModel() {
                     }
         }
     }
-
 }
