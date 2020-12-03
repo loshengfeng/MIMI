b891785f3d92f3700b69be98f12e0ca2b9849f77
neo <neo.chang@silkrode.com.tw>
2020-12-03 19:48:31 +0800

    re-open unread count ui

--- app/src/main/java/com/dabenxiang/mimi/view/personal/PersonalFragment.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/personal/PersonalFragment.kt
@@ -158,7 +158,7 @@ class PersonalFragment : BaseFragment() {
         viewModel.unreadResult.observe(viewLifecycleOwner, Observer {
             when (it) {
                 is Success -> {
-
+                    tv_new.visibility = if (it.result == 0) View.INVISIBLE else View.VISIBLE
                 }
                 is Error -> onApiError(it.throwable)
             }
@@ -167,7 +167,7 @@ class PersonalFragment : BaseFragment() {
         viewModel.totalUnreadResult.observe(viewLifecycleOwner, Observer {
             when (it) {
                 is Success -> {
-
+                    iv_new.visibility = if (it.result == 0) View.INVISIBLE else View.VISIBLE
                     mainViewModel?.refreshBottomNavigationBadge?.value = it.result
                 }
                 is Error -> onApiError(it.throwable)
