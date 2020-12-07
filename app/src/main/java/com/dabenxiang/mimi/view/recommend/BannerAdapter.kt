190ab921d6d6cad0ca299242a4bc04c748178e46
Dave Chao <dave.chao@silkrode.com.tw>
2020-12-07 12:02:15 +0800

    fixed issue 1524

--- app/src/main/java/com/dabenxiang/mimi/view/recommend/BannerAdapter.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/recommend/BannerAdapter.kt
@@ -8,11 +8,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.dabenxiang.mimi.R
 import com.dabenxiang.mimi.model.api.vo.CategoryBanner
 import kotlinx.android.synthetic.main.item_banner.view.*
+import org.json.JSONObject
 
 class BannerAdapter(
     private val banners: List<CategoryBanner>,
     private val bannerFuncItem: BannerFuncItem
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        const val KEY_APP = "app"
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val mView = LayoutInflater.from(parent.context).inflate(R.layout.item_banner, parent, false)
@@ -21,9 +26,12 @@ class BannerAdapter(
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         holder as BannerViewHolder
+
         val banner = banners[position]
         holder.bannerImage.setOnClickListener { bannerFuncItem.onItemClick(banner) }
-        bannerFuncItem.getBitmap(banner.id, holder.bannerImage)
+
+        val bannerId = JSONObject(banner.content).optString(KEY_APP).toLong()
+        bannerFuncItem.getBitmap(bannerId, holder.bannerImage)
     }
 
     override fun getItemCount(): Int {
