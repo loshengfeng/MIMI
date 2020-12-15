c462ed88ec549e98f99e1e90470c2898fbec0bbb
Neo.hu <neo.hu@silkrode.com.tw>
2020-12-15 11:46:35 +0800

    Modify the features that use CODE to Sign in and Sign up

--- app/src/main/java/com/dabenxiang/mimi/model/api/vo/SignInRequest.kt
+++ app/src/main/java/com/dabenxiang/mimi/model/api/vo/SignInRequest.kt
@@ -3,9 +3,12 @@ package com.dabenxiang.mimi.model.api.vo
 import com.google.gson.annotations.SerializedName
 
 class SignInRequest(
-    @SerializedName("username")
-    val userName: String?,
+        @SerializedName("username")
+        val userName: String?,
 
-    @SerializedName("password")
-    val password: String?
+        @SerializedName("code")
+        val code: String?,
+
+        @SerializedName("password")
+        val password: String? = ""
 )
