d219c65da31a58d2f113a0345e7e89f7da2e357b
patrick.lee <patrick@patrickleede-MacBook-Pro.local>
2020-12-22 21:21:59 +0800

    Fixed issue spec #177: [VAI7.1.5.1]訂單建立中新增時間倒數

--- app/src/main/java/com/dabenxiang/mimi/view/orderresult/itemview/OrderResultWaitingItemView.kt
+++ app/src/main/java/com/dabenxiang/mimi/view/orderresult/itemview/OrderResultWaitingItemView.kt
@@ -1,10 +1,13 @@
 package com.dabenxiang.mimi.view.orderresult.itemview
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.airbnb.epoxy.ModelProp
 import com.airbnb.epoxy.ModelView
 import com.dabenxiang.mimi.R
+import kotlinx.android.synthetic.main.item_order_result_waiting.view.*
 
 @ModelView(autoLayout = ModelView.Size.MATCH_WIDTH_MATCH_HEIGHT)
 class OrderResultWaitingItemView(context: Context) : ConstraintLayout(context) {
@@ -13,4 +16,9 @@ class OrderResultWaitingItemView(context: Context) : ConstraintLayout(context) {
         View.inflate(context, R.layout.item_order_result_waiting, this)
     }
 
+    @SuppressLint("SetTextI18n")
+    @ModelProp
+    fun setupCountDown(time: Long) {
+        tv_count_down_hint.text = context.getString(R.string.order_result_create_order_waiting) + "(" + time.toString() + ")"
+    }
 }
